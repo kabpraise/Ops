a89e750c994faeeddfb14797c81ca4f790420717
Kabin <kabpraise@users.noreply.github.com>
2016-12-23 12:59:29 +0100

    Create newlinemedcomma.py

---
+++ SimpleCodes/newlinemedcomma.py
@@ -0,0 +1,30 @@
+'''
+
+Supply filelocation/filename as a single argument
+It will replace new line with comma
+And output will be in filelocation/filename-out
+e.g. $python newlinemedcomma.py /home/ubuntu/test
+will create a file test-out with output
+
+'''
+
+#!/usr/bin/python
+
+import sys
+
+fin = open(str(sys.argv[1]), "r")
+lineout = ""
+i = 0
+
+for line in fin.readlines():
+   line = line.strip()
+   if (i == 1):
+      lineout = lineout + "," + line
+   else:
+      lineout = line
+   i = 1
+
+fin.close()
+fout = open(str(sys.argv[1])+"-out", "w")
+fout.write(lineout)
+fout.close()
